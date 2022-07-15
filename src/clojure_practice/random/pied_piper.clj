25cb375904969ca50239737e357f4277c64ffcc1
Ramesh Sangaraju <ramesh.sangaraju@walmart.com>
2022-07-15 16:05:56 -0500

    cleanup

--- src/clojure_practice/random/pied_piper.clj
+++ src/clojure_practice/random/pied_piper.clj
@@ -40,50 +40,12 @@
       {:left-rats  left-rats
        :right-rats right-rats})))
 
-#_(defn where-is-piper?
-    [input-str]
-    (cond (= (first input-str) \P) :left
-          (= (last input-str) \P) :right
-          :else :random))
-
-#_(defn deaf-rats-count*
-    [rats-count piper-direction]
-    (cond (= piper-direction :left) (-> rats-count
-                                        :right-rats)
-          (= piper-direction :right) (-> rats-count
-                                         :left-rats)))
-#_(defn deaf-rats-on-left-of-piper
-  [rats-count]
-  (-> rats-count
-      :left-rats))
-
-#_(defn deaf-rats-on-right-of-piper
-  [rats-count]
-  (-> rats-count
-      :right-rats))
-
 (defn parse
   [rats-piper-str]
   (let [input-str-with-no-spaces     (str/replace rats-piper-str " " "")
         [left-rats right-rats] (str/split input-str-with-no-spaces #"P" -1)]
     [left-rats right-rats])
   )
-
-#_(defn deaf-rats-count
-  [input]
-  (let [input-str-with-no-spaces     (str/replace input
-                                                  " "
-                                                  "")
-        rats-left-and-right-of-piper (str/split input-str-with-no-spaces
-                                                #"P"
-                                                -1)
-        ;_                               (println "rats-on-left-and-right-of-piper: " rats-on-left-and-right-of-piper)
-        [rats-count-left-of-piper rats-count-right-of-piper] (mapv rats-count
-                                                                   rats-left-and-right-of-piper)
-        ;_                               (println (str rats-count-left-of-piper " P " rats-count-right-of-piper))
-        ]
-    (+ (deaf-rats-on-left-of-piper rats-count-left-of-piper)
-       (deaf-rats-on-right-of-piper rats-count-right-of-piper))))
 
 (defn deaf-rats-count
   [[left-rats right-rats]]
@@ -109,12 +71,6 @@
 
   (rats-direction-count "O~O~~OO~")
   #_=> {:left-rats 3, :right-rats 1}
-
-  ;(deaf-rats-on-right-of-piper {:left-rats 3, :right-rats 1})
-  ;#_=> 1
-  ;
-  ;(deaf-rats-on-left-of-piper {:left-rats 0, :right-rats 0})
-  ;#_=> 0
 
   (deaf-rats-count "PO~O~~OO~")
   #_=> 1
